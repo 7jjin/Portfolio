8b67ba7eaaa2e074a1645f8be717d44676cde72d
7jjin <wlsgud6305@naver.com>
2023-12-17 16:34:25 +0900

    feat 초기설정

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import GlobalStyle from './style/GlobalStyles';
+function App() {
+    return (
+        <div className="App">
+            <GlobalStyle />
+            <h1>hi</h1>
+        </div>
+    );
+}
+
+export default App;
