dc2cd16764c78b574a27ad47ce6e23caac7c988b
7jjin <wlsgud6305@naver.com>
2023-12-28 15:06:53 +0900

    feat stackBtn 대규모 수정
    button -> input으로 바꿈

--- src/component/atoms/input/EtcInput.tsx
+++ src/component/atoms/input/EtcInput.tsx
@@ -1,4 +1,8 @@
 import styled from 'styled-components';
+import figmaImg from '../../../assets/figma.png';
+import postmanImg from '../../../assets/postman.png';
+import awsImg from '../../../assets/aws.png';
+import teachableMachineImg from '../../../assets/teachable.png';
 
 const EtcInput = () => {
     return (
@@ -7,30 +11,24 @@ const EtcInput = () => {
             <_menuOpenButton className="menuOpenButton" htmlFor="etc">
                 Etc
             </_menuOpenButton>
-            <_menuItem className="menuItem react">
-                <span>React</span>
+            <_menuItem className="menuItem figma">
+                <img src={figmaImg} alt="" />
             </_menuItem>
-            <_menuItem className="menuItem typescript">
-                <span>TypeScript</span>
+            <_menuItem className="menuItem postman">
+                <img src={postmanImg} alt="" />
             </_menuItem>
-            <_menuItem className="menuItem reduxToolkit">
-                <span>Redux-toolkit</span>
+            <_menuItem className="menuItem aws">
+                <img src={awsImg} alt="" />
             </_menuItem>
-            <_menuItem className="menuItem recoil">
-                <span>Recoil</span>
-            </_menuItem>
-            <_menuItem className="menuItem zustand">
-                <span>Zustand</span>
-            </_menuItem>
-            <_menuItem className="menuItem styledComponent">
-                <span>Styled-component</span>
+            <_menuItem className="menuItem teachable machine">
+                <img src={teachableMachineImg} alt="" />
             </_menuItem>
         </>
     );
 };
 
 const _menuItem = styled.a`
-    background: #eeeeee;
+    background: #00e961;
     border-radius: 100%;
     width: 80px;
     height: 80px;
@@ -68,15 +66,22 @@ const _menuItem = styled.a`
     &:nth-child(9) {
         transition-duration: 180ms;
     }
+    & img {
+        object-fit: cover;
+        border-radius: 100%;
+        width: 100%;
+        height: 100%;
+    }
 `;
 const _menuOpenButton = styled.label`
-    background: #eeeeee;
+    background: #00e961;
     border-radius: 100%;
     width: 80px;
     height: 80px;
     margin-left: -40px;
     position: absolute;
-    color: #ffffff;
+    color: black;
+    font-weight: 600;
     text-align: center;
     line-height: 80px;
     transform: translate3d(0, 0, 0);
@@ -97,37 +102,30 @@ const _menuOpen = styled.input`
         transition-timing-function: linear;
         transition-duration: 200ms;
         transform: scale(0.8, 0.8) translate3d(0, 0, 0);
+        background: white;
     }
     &:checked ~ .menuItem {
         transition-timing-function: cubic-bezier(0.935, 0, 0.34, 1.33);
     }
     &:checked ~ .menuItem:nth-child(3) {
         transition-duration: 180ms;
-        transform: translate3d(0.08361px, -104.99997px, 0);
+        transform: translate3d(-65.91639px, -67.99997px, 0);
+        box-shadow: 3px 3px 0 0 rgba(0, 0, 0, 0.14);
     }
     &:checked ~ .menuItem:nth-child(4) {
         transition-duration: 280ms;
-        transform: translate3d(90.9466px, -52.47586px, 0);
+        transform: translate3d(65.91639px, -67.99997px, 0);
+        box-shadow: 3px 3px 0 0 rgba(0, 0, 0, 0.14);
     }
     &:checked ~ .menuItem:nth-child(5) {
         transition-duration: 380ms;
-        transform: translate3d(90.9466px, 52.47586px, 0);
+        transform: translate3d(65.91639px, 67.99997px, 0);
+        box-shadow: 3px 3px 0 0 rgba(0, 0, 0, 0.14);
     }
     &:checked ~ .menuItem:nth-child(6) {
         transition-duration: 480ms;
-        transform: translate3d(0.08361px, 104.99997px, 0);
-    }
-    &:checked ~ .menuItem:nth-child(7) {
-        transition-duration: 580ms;
-        transform: translate3d(-90.86291px, 52.62064px, 0);
-    }
-    &:checked ~ .menuItem:nth-child(8) {
-        transition-duration: 680ms;
-        transform: translate3d(-91.03006px, -52.33095px, 0);
-    }
-    &:checked ~ .menuItem:nth-child(9) {
-        transition-duration: 780ms;
-        transform: translate3d(-0.25084px, -104.9997px, 0);
+        transform: translate3d(-65.91639px, 67.99997px, 0);
+        box-shadow: 3px 3px 0 0 rgba(0, 0, 0, 0.14);
     }
 `;
 
