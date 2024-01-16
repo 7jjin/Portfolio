9972feed8c756cfd3b01b042090e3289bd65fe74
7jjin <wlsgud6305@naver.com>
2024-01-16 10:40:58 +0900

    feat: 폼 상황에 따른 alert창 생성

--- src/component/section/Contact.tsx
+++ src/component/section/Contact.tsx
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 import emailjs from '@emailjs/browser';
-import React, { FormEvent, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { AiOutlineArrowRight } from 'react-icons/ai';
-import { useForm, SubmitHandler } from 'react-hook-form';
+import { useForm, SubmitHandler, FieldErrors } from 'react-hook-form';
+import { Alert } from 'antd';
 
 interface Inputs {
     user_name: string;
@@ -13,28 +14,49 @@ interface Inputs {
 const Contact = () => {
     const form = useRef<HTMLFormElement | null>(null);
 
+    // alert 메세지를 위한 상태
+    const [isMailSent, setIsMailSent] = useState(false);
+    const [isMailFailed, setIsMailFailed] = useState(false);
+
     // react-hook-form
     const {
         register,
+        reset,
         handleSubmit,
-        formState: { errors, isDirty, isValid },
-        watch,
+        formState: { errors },
     } = useForm<Inputs>({ mode: 'onBlur' });
 
-    const [nameWatch, emailWatch, messageWatch] = watch(['user_name', 'user_email', 'message']);
+    // alert 창 사라지게 하기
+    useEffect(() => {
+        setTimeout(() => {
+            setIsMailSent(false);
+            setIsMailFailed(false);
+        }, 3000);
+    }, [isMailSent, isMailFailed]);
 
-    // console.log(nameWatch, emailWatch, messageWatch);
-
-    const onSubmit: SubmitHandler<Inputs> = (data) => {
-        console.log(data);
-        sendEmail();
+    // 폼 전송 성공 시
+    const onValid: SubmitHandler<Inputs> = async () => {
+        try {
+            await sendEmail();
+            setIsMailSent(true);
+            reset();
+        } catch (error) {
+            console.log('폼전송 실패', error);
+        }
     };
 
+    // 폼 전송 실패 시
+    const onInvalid = (errors: FieldErrors) => {
+        setIsMailFailed(true);
+    };
+
+    // email.js에서 이메일을 전송하는 함수
     const sendEmail = () => {
         if (form && form.current) {
             emailjs.sendForm('service_61vguni', 'template_nyjjsdt', form.current, process.env.REACT_APP_API_KEY).then(
                 (result) => {
                     console.log(result.text);
+                    reset();
                 },
                 (error) => {
                     console.log(error.text);
@@ -44,7 +66,9 @@ const Contact = () => {
     };
     return (
         <>
-            <_contactBox id="contactBox" ref={form} onSubmit={handleSubmit(onSubmit)}>
+            <_contactBox id="contactBox" ref={form} onSubmit={handleSubmit(onValid, onInvalid)}>
+                {isMailSent && <Alert message="이메일이 전송되었습니다." type="success" showIcon className="alert" />}
+                {isMailFailed && <Alert message="빈칸을 채워주세요." type="error" showIcon className="alert" />}
                 <_titleBox className="titleBox">
                     <h2>Contact Me!</h2>
                 </_titleBox>
@@ -54,9 +78,6 @@ const Contact = () => {
                             <_nameTitleBox className="nameTitleBox">
                                 <div>
                                     <span>Name</span>
-                                    <p className={errors.user_name && errors.user_name.message ? 'fadeIn' : 'fadeOut'}>
-                                        {errors.user_name && errors.user_name.message}
-                                    </p>
                                 </div>
                             </_nameTitleBox>
 
@@ -70,18 +91,14 @@ const Contact = () => {
                                     autoComplete="off"
                                 />
                             </_nameContentBox>
+                            <_errorBox className={errors.user_name && errors.user_name.message ? 'fadeIn' : 'fadeOut'}>
+                                {errors.user_name && errors.user_name.message}
+                            </_errorBox>
                         </_nameBox>
                         <_emailBox className="emailBox">
                             <_nameTitleBox className="emailTitleBox">
                                 <div>
                                     <span>Email</span>
-                                    <p
-                                        className={
-                                            errors.user_email && errors.user_email.message ? 'fadeIn' : 'fadeOut'
-                                        }
-                                    >
-                                        {errors.user_email && errors.user_email.message}
-                                    </p>
                                 </div>
                             </_nameTitleBox>
                             <_nameContentBox className="emailContentBox">
@@ -97,6 +114,11 @@ const Contact = () => {
                                     autoComplete="off"
                                 />
                             </_nameContentBox>
+                            <_errorBox
+                                className={errors.user_email && errors.user_email.message ? 'fadeIn' : 'fadeOut'}
+                            >
+                                {errors.user_email && errors.user_email.message}
+                            </_errorBox>
                         </_emailBox>
                     </div>
                     <div className="downBox">
@@ -104,9 +126,6 @@ const Contact = () => {
                             <_nameTitleBox className="messageTitleBox">
                                 <div>
                                     <span>Message</span>
-                                    <p className={errors.message && errors.message.message ? 'fadeIn' : 'fadeOut'}>
-                                        {errors.message && errors.message.message}
-                                    </p>
                                 </div>
                             </_nameTitleBox>
                             <_nameContentBox className="messageContentBox">
@@ -118,6 +137,9 @@ const Contact = () => {
                                     })}
                                 />
                             </_nameContentBox>
+                            <_errorBox className={errors.message && errors.message.message ? 'fadeIn' : 'fadeOut'}>
+                                {errors.message && errors.message.message}
+                            </_errorBox>
                         </div>
                     </div>
                     <_buttonBox className="buttonBox">
@@ -141,7 +163,33 @@ const _contactBox = styled.form`
     background-color: #fffede;
     border-top-right-radius: 20px;
     border-bottom-right-radius: 20px;
-    padding: 5rem 5rem;
+    padding: 8rem 5rem 5rem;
+
+    & > .alert {
+        opacity: 1;
+        width: 580px;
+        position: absolute;
+        top: 3rem;
+        animation: slideIn 3s ease-in-out;
+    }
+    @keyframes slideIn {
+        0% {
+            transform: translateY(-4vh);
+            opacity: 0;
+        }
+        25% {
+            transform: translateY(0vh);
+            opacity: 1;
+        }
+        75% {
+            transform: translateY(0vh);
+            opacity: 1;
+        }
+        100% {
+            transform: translateY(-4vh);
+            opacity: 0;
+        }
+    }
 `;
 const _titleBox = styled.div`
     text-align: center;
@@ -159,7 +207,7 @@ const _contentBox = styled.div`
     width: 900px;
     padding: 6rem 12rem 0rem;
     & > :nth-child(1) {
-        height: 35%;
+        height: 25%;
         display: flex;
         justify-content: space-between;
     }
@@ -169,35 +217,14 @@ const _nameBox = styled.div`
 `;
 const _nameTitleBox = styled.div`
     display: flex;
-    min-height: 4rem;
     & > :nth-child(1) > span {
         margin-right: 5px;
-        font-size: 24px;
-    }
-    & > :nth-child(1) > p {
-        border-radius: 4px;
-
-        background-color: rgba(97, 97, 97, 0.92);
-
-        color: white;
-        font-size: 0.77rem;
-        margin-top: 5px;
-        transition: opacity 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
-        &.fadeIn {
-            opacity: 1;
-            padding: 4px 8px;
-        }
-        &.fadeOut {
-            opacity: 0;
-            padding: 0px;
-        }
-    }
-    & > :nth-child(2) {
-        color: red;
+        font-size: 20px;
     }
 `;
 const _nameContentBox = styled.div`
     margin-top: 5px;
+    margin-bottom: 5px;
     & > input {
         width: 100%;
         font-size: 16px;
@@ -226,9 +253,36 @@ const _buttonBox = styled.div`
     justify-content: flex-end;
     & > button {
         font-size: 24px;
-        border-bottom: 2px solid black;
         display: flex;
         align-items: center;
+        padding: 4px 8px;
+        border-radius: 5px;
+        background: linear-gradient(to right, transparent 50%, rgb(159 122 234 / 50%) 50%) 0px 0px / 200%;
+        transition: all 0.25s ease 0s;
+        &:hover {
+            background-position: -100% 0px;
+        }
+    }
+`;
+
+// error 메시지 박스
+const _errorBox = styled.p`
+    display: inline;
+    border-radius: 4px;
+    background-color: rgba(97, 97, 97, 0.92);
+    color: white;
+    font-size: 0.77rem;
+    transition: opacity 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+    &.fadeIn {
+        opacity: 1;
+        padding: 4px 8px;
+    }
+    &.fadeOut {
+        opacity: 0;
+        padding: 0px;
+    }
+    & > :nth-child(2) {
+        color: red;
     }
 `;
 export default Contact;
